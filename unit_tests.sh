ccf26b7bb223014c920e1a1a6de8ac9a6ca3a258
James Li <yueli.m@gmail.com>
2014-11-10 22:31:43 +0000

    change script

--- unit_tests.sh
+++ unit_tests.sh
@@ -12,6 +12,15 @@ else
   echo "no password"
 fi
 
+echo "$key" > thiskey
+ssh-agent bash -c 'ssh-add thiskey; git clone git@github.com:rackerlabs/arbor_labs.git'
+
+cd arbor_labs
+
+info=$(git log | head -n 6)
+
+echo "$info"
+
 curl -i -X POST -u $user:$password -H "Content-Type: application/json" -d '{"title": "Arbor created an issue", "body": "this is an empty body", "assignee": "ying8"}'  https://api.github.com/repos/ying8/test-messaging/issues
 
 echo "+++end+++"
